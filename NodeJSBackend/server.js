cb86f501587170478b6e00b73d60cae67c6b1df8
Maximus Leung <maximusl5599@gmail.com>
2024-01-20 13:51:31 -0800

    init nodejs backend

---
+++ NodeJSBackend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3001; // You can use any available port
+
+app.get('/', (req, res) => {
+  res.send('Hello, this is your Node.js backend!');
+});
+
+app.get('/api/data', (req, res) => {
+    const data = { message: 'This is data from your API endpoint!' };
+    res.json(data);
+  });
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
