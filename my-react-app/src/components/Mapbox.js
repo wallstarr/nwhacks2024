04f92cdeeb3fdb9fd3b37f2e1f9d5876ee485778
Maximus Leung <maximusl5599@gmail.com>
2024-01-20 22:11:28 -0800

    updated mapbox

--- my-react-app/src/components/Mapbox.js
+++ my-react-app/src/components/Mapbox.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
+import axios from 'axios';
 import { MappedIn } from './MappedIn';
 import './Mapbox.css'
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export const Mapbox = () => {
 
@@ -10,16 +12,50 @@ export const Mapbox = () => {
     const mapContainer = useRef(null);
     const map = useRef(null);
 
+    const placesData = useRef(null)
+    const [selectedPlaceDetails, setSelectedPlaceDetails] = useState(null); // State to store selected place details
+
     useEffect(() => {
         if (map.current) return;
         mapboxgl.accessToken = 'pk.eyJ1IjoibWF4aW11c2w1OSIsImEiOiJjbHJtandqdXQwd2dvMmpvZnphOHRvMm9jIn0.XMUyvUmW_sGAUEugPnjTxg';
         map.current = new mapboxgl.Map({
             container: mapContainer.current, // container ID
             style: 'mapbox://styles/mapbox/streets-v12', // style URL
-            center: [-74.5, 40], // starting position [lng, lat]
-            zoom: 9, // starting zoom
+            center: [-123.117, 49.238], // starting position [lng, lat]
+            zoom: 12, // starting zoom
         });
     });
+
+    axios.get('http://localhost:3000/places-coordinates')
+        .then(response => {
+            placesData.current = response.data
+            // Ensure the map instance is available
+            if (!map.current) return;
+            addMarkersToMap(placesData.current);
+        })
+        .catch(error => console.error('Error fetching data: ', error));
+
+    const addMarkersToMap = (placesData) => {
+        console.log(placesData)
+        placesData.forEach(place => {
+            const marker = new mapboxgl.Marker()
+                .setLngLat([place.longitude, place.latitude])
+                .addTo(map.current);
+
+            marker.getElement().addEventListener('click', () => {
+                console.log("here")
+                fetchPlaceDetailsById(place.placeId); // Fetch details when marker is clicked
+            });
+        });
+    };
+
+    const fetchPlaceDetailsById = (placeId) => {
+        axios.get('http://localhost:3000/place-details?placeId=${placeId}')
+                .then(res => {
+            setSelectedPlaceDetails(res.data); // Update state with fetched details
+        })
+            .catch(error => console.error('Error fetching details:', error));
+    };
 
     useEffect(() => {
         setTimeout(performAction, 5000);
@@ -32,8 +68,9 @@ export const Mapbox = () => {
 
     return (
         <div>
-            <div ref={mapContainer} id='map' className='map transition' style={{opacity: showMappedIn ? '0' : '100' }}></div>
-            <div className='transition' style={{opacity: showMappedIn ? '100' : '0' }}><MappedIn/></div>
+            <div ref={mapContainer} id='map' className='map transition' style={{ opacity: showMappedIn ? '0' : '100' }}></div>
+            <div className='transition' style={{ opacity: showMappedIn ? '100' : '0' }}><MappedIn /></div>
+            {selectedPlaceDetails && <MapCard feature={selectedPlaceDetails} />}
         </div>
     )
 }
